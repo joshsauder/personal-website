2584b15020b6930c3245dcb01e78e61c939596ae
joshsauder <joshsauder@gmail.com>
2019-01-21 13:47:23 -0500

    Created new web project with Navigation bar complete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+class Navigation extends React.Component {
+    render() {
+        return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">
+            <img src={logo} className="d-block" width="36" height="36" alt="logo" />
+        </a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Josh Sauder<span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">About Me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">My Projects</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+        )
+    }
+
+}
+
+export default Navigation;
