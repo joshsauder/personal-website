6327b626f67d0cd171b01fb440363408d1235a34
joshsauder <joshsauder@gmail.com>
2019-02-01 21:49:25 -0500

    Added \about content. still need styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import InTheClear from '../InTheClear.png';
+import ReactLogo from '../React.png';
 import '../App.css';
 import Navigation from './Navigation';
 import AboutMe from './AboutMe'
@@ -9,7 +10,41 @@ class About extends Component {
     return (
       <div>
         <Navigation />
-        <p>About Me</p>
+        <div className="featurette-divider"></div>
+        <div className="container marketing">
+          <div className="row featurette">
+                <div className="col-md-7">
+                    <h2 className="featurette-heading">In The Clear</h2>
+                    <p className="lead">Allows the user to determine what weather conditions they will be experiencing during their travels</p>
+                    <p className="lead">Includes a drop-down menu that shows the user, city by city, what weather and what temperatures they should expect</p>
+                    <p className="lead">I've always had a fascination for meterology. A common problem with living in the midwest is that when you travel, you can experience a variety of different weather conditions. This app solves this issue by telling the user what types of weather they will experience during their travels.</p>
+                    <p className="lead">Built using Swift</p>
+                    <p className="lead">Podfiles used include Google Maps, Google Places, AlamoFire, and SwiftlyJSON</p>
+                    <button type="button" class="btn btn-success">Request Access of Source Code</button>
+                    <button type="button" class="btn btn-warning">Feedback</button>
+                </div>
+                <div className="col-md-5">
+                    <img className="featurette-image img-fluid mx-auto" src={InTheClear}></img>
+                </div>
+          </div>
+
+          <hr className="featurette-divider"/>
+
+          <div className="row featurette">
+              <div className="col-md-7 order-md-2">
+                  <h2 className="featurette-heading">Personal Website</h2>
+                  <p className="lead">A website that shows what I've done both professionally and in my free time</p>
+                  <p className="lead">I've always enjoyed web development and a personal web page is something that I thought wouldn't be a bad thing to have</p>
+                  <p className="lead">It's basically a great way to find out what I've done, and an easy way to get to know me.</p>
+                  <p className="lead">Build using ReactJS, Bootstrap, Webpack, and NodeJS</p>
+                  <button type="button" class="btn btn-success">Request Access of Source Code</button>
+                  <button type="button" class="btn btn-warning">Feedback</button>
+              </div>
+              <div className="col-md-5">
+                  <img className="featurette-image img-fluid mx-auto" src={ReactLogo}></img>
+              </div>
+          </div>
+        </div>
       </div>
     );
   }
