d2605e0519e2cc1ea6e0e4fe8fca9c9f1421382b
Josh Sauder <joshsauder@gmail.com>
2019-05-20 22:43:29 -0400

    Updated logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -12,10 +12,10 @@ class Navigation extends React.Component {
         <div className="nav-fixed-top">
         <Navbar variant="dark" expand="md" className="bg-darkPurple">
           <Navbar.Brand href="/">
-            <img src={logo} className="d-block" width="36" height="36" alt="logo" />
+            <img src={logo} className="d-block" height="60" alt="logo" />
           </Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="/" active>Josh Sauder</Nav.Link>
+            <Nav.Link href="/" active>Home</Nav.Link>
             <Nav.Link href="/about" active>About Me</Nav.Link>
             <Nav.Link href="/projects" active>My Projects</Nav.Link>
           </Nav>
