c5d79fe0d34a2ee6848dd70a3e5e153d436a3004
Josh Sauder <joshsauder@gmail.com>
2019-05-20 21:56:09 -0400

    updated home page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,6 +3,7 @@ import InTheClear from '../InTheClear.png';
 import ReactLogo from '../React.png';
 import Cover from '../CoverPhoto.png'
 import Navigation from './Navigation';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 class AboutMe extends React.Component {
     render(){
@@ -16,27 +17,30 @@ class AboutMe extends React.Component {
                 </div>
               </div>
               <div className="container marketing">
-                <div className="row featurette">
-                      <div className="col-md-7">
-                          <h2 className="featurette-heading">In The Clear</h2>
-                          <p className="lead">An iOS app built that allows the user to get weather information during there travel.</p>
-                      </div>
-                      <div className="col-md-5">
-                          <img className="featurette-image img-fluid mx-auto" src={InTheClear}></img>
-                      </div>
-                </div>
+                <Jumbotron>
+                  <div className="row featurette">
+                        <div className="col-md-7">
+                            <h2 className="featurette-heading">In The Clear</h2>
+                            <p className="lead">An iOS app built that allows the user to get weather information during there travel.</p>
+                        </div>
+                        <div className="col-md-5 featurette-img-align">
+                            <img className="featurette-image img-fluid mx-auto" src={InTheClear}></img>
+                        </div>
+                  </div>
+                </Jumbotron>
 
                 <hr className="featurette-divider"/>
-
-                <div className="row featurette">
-                    <div className="col-md-7 order-md-2">
-                        <h2 className="featurette-heading">Personal Website</h2>
-                        <p class="lead">A website I built using React that shows what I have done professionally and in my free time</p>
-                    </div>
-                    <div className="col-md-5">
-                        <img className="featurette-image img-fluid mx-auto" src={ReactLogo}></img>
-                    </div>
-                </div>
+                <Jumbotron>
+                  <div className="row featurette">
+                      <div className="col-md-7 order-md-2">
+                          <h2 className="featurette-heading">Personal Website</h2>
+                          <p class="lead">A website I built using React that shows what I have done professionally and in my free time</p>
+                      </div>
+                      <div className="col-md-5 featurette-img-align">
+                          <img className="featurette-image img-fluid mx-auto" src={ReactLogo}></img>
+                      </div>
+                  </div>
+                </Jumbotron>
               </div>
             </div>
         )
