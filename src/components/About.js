50199f7ae34f0eec345555ece94c9f4e2f553ddb
Josh Sauder <joshsauder@gmail.com>
2019-05-27 21:53:42 -0400

    added column constraints to featurretes

--- src/components/About.js
+++ src/components/About.js
@@ -34,53 +34,74 @@ class About extends Component {
         <h1 className="text-center text-white mt-5">About Me</h1>
         <div className="container marketing">
         <div className="row ">
-        <div className= "col-md-8 ml-3">
+        <div className= "col-md-8 col-8 ml-3">
             <div className="featurette">
               <Jumbotron className="row">
-                  <div className="col-md-7">
+                  <div className="col-md-7 col-7">
                       <h2 className="featurette-heading">Professionally</h2>
-                      <p>My goal, as a developer, is to create applications that better the users life. I truely believe applications can positively impact a persons life and help make the world a better place.</p>
-                      <p>I've always been a curious person and learning a new skill has never been something I've shied away from. It always me to increase my knowledge set, and broadens my horizons.</p>
+                      <p className="lead">My goal, as a developer, is to create applications that better the users life. I truely believe applications can positively impact a persons life and help make the world a better place.</p>
+                      <p className="lead">I've always been a curious person and learning a new skill has never been something I've shied away from. It always me to increase my knowledge set, and broadens my horizons.</p>
                   </div>
-                  <div className="col-md-5 featurette-img-align">
+                  <div className="col-md-5 col-5 featurette-img-align">
                       <img className="featurette-image img-fluid mx-auto rounded" src={GradPhoto}></img>
                   </div>
                 </Jumbotron>
             </div>
         </div>
-        <div className= "col-md-3 ml-3">
+        <div className= "col-md-3 ml-4 col-3">
             <div className="row featurette">
               <Jumbotron>
-              <div className="col-md-12">
+              <div className="col-md-12 col-12">
                 <h2 className="featurette-heading">Experience</h2>
                 <button type ="button" className="btn btn-clear" onClick={() => this.setState({ OSUShow: true })}>B.S. Computer Science Engineering - The Ohio State University</button>
                 <button type ="button" className="btn btn-clear" onClick={() => this.setState({ NTIShow: true })}>Software Engineer - Network Technologies International</button>
                 <button type ="button" className="btn btn-clear" onClick={() => this.setState({ CASShow: true })}>Chemical Abstract Services - Software Engineering Co-Op</button>
-                <button type = "button" className="btn btn-clear" onClick={() => this.setState({ XPOShow: true })}>XPO Logistics - IT Intern</button>
+                <button type ="button" className="btn btn-clear" onClick={() => this.setState({ XPOShow: true })}>XPO Logistics - IT Intern</button>
               </div>
               </Jumbotron>
             </div>
         </div>
-        </div>
-
-          <div className= "shadow mb-3">
-            <Jumbotron>
+      </div>  
+      <div className="row">
+          <div className="col-md-7 col-7 ml-3">
+            <div className="featurette">
+              <Jumbotron className="row">
+              <h2 className="featurette-heading">What I Know</h2>
+                <p className="lead">Languages and Frameworks - Javascript, AngularJS, HTML5, CSS, Bootstrap, ExpressJS, MSSQL, MySQL, Java, C++, Swift</p>
+                <p className="lead">IDE/Text Editor - XCode, IntelliJ, Visual Studio Code, Eclipse</p>
+                <p className="lead">Version Control - Git</p>
+                <p className="lead">Project Management - Jira, Trello, Mantis</p>
+              </Jumbotron>
+            </div>
+          </div>
+          <div className="col-md-4 col-4 ml-4">
+            <div className="featurette">
+              <Jumbotron className="row">
+                <h2 className="featurette-heading">What I'm Learning</h2>
+                <p className="lead">Currently - NodeJS, ReactJS, XCTest</p>
+                <p className="lead">Upcoming - MongoDB, GraphQL, Gulp</p>
+              </Jumbotron>
+            </div>
+          </div> 
+      </div>
+      <div className= "shadow mb-3">
+          <Jumbotron>
             <div className="row featurette">
-                  <div className="col-md-7">
+                  <div className="col-md-6 col-6">
                       <h2 className="featurette-heading">Personally</h2>
                       <p className="lead">Born in Pettisville, OH (Toledo area). Graduated from Pettisville with a class of 50. Pretty small right!</p>
                       <p className="lead">In my free-time you'll most likely find me, outside running. I'm sort of a self-proclaimed running fanatic. I've run 2 full marathons, 3 half marathons, and numerous 5ks and 10ks. If you're wondering what I'm doing after work, there's a good chance I'm out running.</p>
                       <p className="lead">I also love traveling and exploring new places. I've always enjoyed venturing to new places and trying new foreign cuisines.</p>
                       <p className="lead">Huge Ohio State Buckeye fan. I love watching OSU football and basketball whether I'm at the game, or watching it on TV.</p>
                   </div>
-                  <div className="col-md-5 featurette-img-align">
+                  <div className="col-md-5 col-5 featurette-img-align">
                       <img className="featurette-image img-fluid mx-auto rounded" src={Selfie}></img>
                   </div>
               </div>
-              </Jumbotron>
-            </div>
-          <hr className="featurette-divider"/>
-        </div>
+          </Jumbotron>
+      </div>
+      <hr className="featurette-divider"/>
+      </div>
       </div>
     );
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,7 +25,7 @@ class Projects extends Component {
         <div className= "shadow mb-5">
           <Jumbotron>
           <div className="row featurette">
-                <div className="col-md-7">
+                <div className="col-md-7 col-7">
                     <h2 className="featurette-heading">In The Clear</h2>
                     <p className="lead">Allows the user to determine what weather conditions they will be experiencing during their travels</p>
                     <p className="lead">Includes a drop-down menu that shows the user, city by city, what weather and what temperatures they should expect</p>
@@ -38,7 +38,7 @@ class Projects extends Component {
                       <img src={appStore}></img>
                     </button>
                 </div>
-                <div className="col-md-5 featurette-img-align">
+                <div className="col-md-5 col-5 featurette-img-align">
                     <img className="featurette-image img-fluid mx-auto rounded" src={InTheClear}></img>
                 </div>
             </div>
@@ -47,7 +47,7 @@ class Projects extends Component {
           <div className= "shadow mb-5">
             <Jumbotron>
             <div className="row featurette">
-                <div className="col-md-7 order-md-2">
+                <div className="col-md-7 col-7 order-md-2">
                     <h2 className="featurette-heading">Personal Website</h2>
                     <p className="lead">A website that shows what I've done both professionally and in my free time</p>
                     <p className="lead">I've always enjoyed web development and a personal web page is something that I thought wouldn't be a bad thing to have</p>
@@ -56,7 +56,7 @@ class Projects extends Component {
                     <button type="button" className="btn btn-dark mr-3" onClick={() => this.setState({ show: true, title: "Website Request"})}>Source Code Request</button>
                     <button type="button" className="btn btn-dark" onClick={() => this.setState({ show: true, title: "Website Feedback"})}>Feedback</button>
                 </div>
-                <div className="col-md-5 featurette-img-align">
+                <div className="col-md-5 col-5 featurette-img-align">
                     <img className="featurette-image img-fluid mx-auto rounded" src={ReactLogo}></img>
                 </div>
             </div>
