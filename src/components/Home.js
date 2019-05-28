4e31c086459c666729d4b5c3d253c0c59021a2ac
Josh Sauder <joshsauder@gmail.com>
2019-05-27 20:31:40 -0400

    added titles to buttons for mouseover

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,11 +19,11 @@ class Home extends Component {
               <div className="mt-8">
                 <img className="mx-auto d-block img-responsive" src={Logo}></img>
                 <div className="row justify-content-center">
-                  <div className="col-sm-2 col-2">
-                    <a href="mailto:joshsauder@gmail.com"><i className="fa fa-envelope fa-lg btn-social"></i></a>
+                  <div className="col-1">
+                    <a href="mailto:joshsauder@gmail.com" Title="Email"><i className="fa fa-envelope fa-lg btn-social"></i></a>
                   </div>
-                  <div className="col-sm-2 col-2">
-                    <a href="www.linkedin.com/in/josh-sauder-5231a1106"><i className="fa fa-linkedin fa-lg btn-social"></i></a>
+                  <div className="col-1">
+                    <a href="www.linkedin.com/in/josh-sauder-5231a1106" title="LinkedIn Profile"><i className="fa fa-linkedin fa-lg btn-social"></i></a>
                   </div>
                 </div>
                 <h2 className="text-center text-white mt-5">Let&rsquo;s build something that brings a positive impact on the world!</h2>
