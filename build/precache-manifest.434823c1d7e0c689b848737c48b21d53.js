87965b1172b89bec2a7c196b2aeeccc051f80e08
Josh Sauder <joshsauder@gmail.com>
2019-06-02 21:27:35 -0400

    need build for netlify tto work

---
+++ build/precache-manifest.434823c1d7e0c689b848737c48b21d53.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "57e3a0b2fa01a23f056029ac92d8392a",
+    "url": "/static/media/SelfImage.57e3a0b2.png"
+  },
+  {
+    "revision": "2fc64ce9ba673c00e4b4",
+    "url": "/static/css/main.a6c07845.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "bdadd676b72bbc327315",
+    "url": "/static/js/1.bdadd676.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "617a26f0d3494580332693946f759bcf",
+    "url": "/static/media/logo.617a26f0.png"
+  },
+  {
+    "revision": "a37584e7069ab1e068650814649c9002",
+    "url": "/static/media/InTheClear.a37584e7.png"
+  },
+  {
+    "revision": "6d76a61481458c930f3f52464b72a637",
+    "url": "/static/media/IMG_0138.6d76a614.png"
+  },
+  {
+    "revision": "2fc64ce9ba673c00e4b4",
+    "url": "/static/js/main.2fc64ce9.chunk.js"
+  },
+  {
+    "revision": "a005f853bcf64cb36bb3dc587f742aee",
+    "url": "/static/media/logoScreenShot.a005f853.png"
+  },
+  {
+    "revision": "2928664fe1fc6aca88583a6f606d60ba",
+    "url": "/static/media/appStore.2928664f.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "bdadd676b72bbc327315",
+    "url": "/static/css/1.f41e1a2a.chunk.css"
+  },
+  {
+    "revision": "3a5f9d68eaa975d412514d117a49dd33",
+    "url": "/index.html"
+  }
+];
