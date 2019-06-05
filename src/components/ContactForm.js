fce94c36b96c702c46c968f45c1de03e4d7e9102
Josh Sauder <joshsauder@gmail.com>
2019-06-04 21:59:26 -0400

    added max length and fixed bug that allowed user to email blast

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,6 +5,49 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import $ from 'jquery';
 
+function ToggleFormAndAlert(props) {
+  if(props.success === true){
+    return (
+      <Alert variant="success">
+        <Alert.Heading>Your Email Successfully Sent!</Alert.Heading>
+          <p>
+            Thank you for your interest! You should recieve an email from me reguarding your request within the next 48 hours. 
+         </p>        
+      </Alert>
+    );
+  } else if (props.fail === true){
+    return (
+      <Alert variant="danger">
+        <Alert.Heading>Uhh Oh! Looks like there was an issue sending your email.</Alert.Heading>
+        <p>
+          Sorry for the inconvience. You can email me directly at joshsauder@gmail.com reguarding your request.
+        </p>
+      </Alert>
+    )
+  } else {
+      return (
+        <form>
+          <div className="form-group">
+            <label htmlFor="name" className="col-form-label">Name</label>
+            <input type="text" className="form-control" id="name" maxLength="70"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email" className="col-form-label">Email</label>
+            <input type="text" className="form-control" id="email" maxLength="254"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="organization" className="col-form-label">Organization (optional)</label>
+            <input type="text" className="form-control" id="organization"maxLength="100"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="comments" className="col-form-label">Comments (optional)</label>
+            <textarea className="form-control" name="comments" id="comments" maxLength="400" placeholder="Maximun Length: 400 Characters"></textarea>
+          </div>
+        </form> 
+      )
+  }
+}
+
 class ContactForm extends Component {
 
   constructor(props) {
@@ -12,7 +55,8 @@ class ContactForm extends Component {
     this.state = { 
       show: props.modal,
       successAlert: props.alert,
-      failAlert: props.alert
+      failAlert: props.alert,
+      sentMail: false
     };
 
   }
@@ -22,7 +66,8 @@ class ContactForm extends Component {
         return {
             show: nextProps.modal,
             successAlert: nextProps.alert,
-            failAlert: nextProps.alert
+            failAlert: nextProps.alert,
+            sentMail: false
         }
     }else {
         return null;
@@ -30,6 +75,7 @@ class ContactForm extends Component {
 }
 
   handleClick(props) {
+    this.setState({ sentMail: true })
     var name = $('#name').val();
     var email = $('#email').val();
     var org = $('#organization').val();
@@ -49,7 +95,6 @@ class ContactForm extends Component {
       })
     })
     .then((response)=> {
-      console.log(response.status);
       if (response.status === 200){
         this.setState({
           successAlert: true
@@ -69,6 +114,12 @@ class ContactForm extends Component {
     )
   }
 
+  shouldDisplayAlert() {
+    return( 
+      this.state.successAlert || this.state.failAlert
+    )
+  }; 
+
   render() {
     return (
       <Modal show={this.state.show} onHide={this.props.onHide} onClose={this.props.onClose}>
@@ -76,47 +127,17 @@ class ContactForm extends Component {
             <Modal.Title>{this.props.title} Form</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Alert variant="success" show={this.state.successAlert}>
-              <Alert.Heading>Your Email Successfully Sent!</Alert.Heading>
-              <p>
-                Thank you for your interest! You should recieve an email from me reguarding your request within the next 48 hours. 
-              </p>
-            </Alert>
-            <Alert variant="danger" show={this.state.failAlert}>
-              <Alert.Heading>Uhh Oh! Looks like there was an issue sending your email.</Alert.Heading>
-              <p>
-                Sorry for the inconvience. You can email me directly at joshsauder@gmail.com reguarding your request.
-              </p>
-            </Alert>
-            <form>
-            <div className="form-group">
-              <label htmlFor="name" className="col-form-label">Name</label>
-              <input type="text" className="form-control" id="name"/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="email" className="col-form-label">Email</label>
-              <input type="text" className="form-control" id="email"/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="organization" className="col-form-label">Organization (optional)</label>
-              <input type="text" className="form-control" id="organization" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="comments" className="col-form-label">Comments (optional)</label>
-              <textarea className="form-control" name="comments" id="comments"></textarea>
-            </div>
-            </form>
+            <ToggleFormAndAlert success={this.state.successAlert} fail={this.state.failAlert} />
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="primary" onClick={() => { this.handleClick(this.props) }} >
+            <Button variant="primary" onClick={() => { this.handleClick(this.props) }} disabled={this.state.sentMail} >
               Submit
             </Button>
             <Button variant="primary" onClick={this.props.onHide} >
               Close
             </Button>
-          </Modal.Footer>
+          </Modal.Footer>        
         </Modal>
-
     );
   }
 }
