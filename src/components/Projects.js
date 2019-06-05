e98b5e68c83e3bb996d10ce56246a8fac1f1f96b
Josh Sauder <joshsauder@gmail.com>
2019-06-04 22:57:09 -0400

    modified filled logo in projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../App.css';
 import Slide from 'react-reveal/Slide';
 import InTheClear from '../images/InTheClear.png';
-import logoShot from '../images/logoScreenShot.png';
+import logoShot from '../images/logoShot.png';
 import appStore from '../images/appStore.svg'
 import ContactForm from './ContactForm';
 import Jumbotron from 'react-bootstrap/Jumbotron';
