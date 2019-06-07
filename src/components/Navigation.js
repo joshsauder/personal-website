9243c343135081c9eebee4c5e1c960453ba24019
Josh Sauder <joshsauder@gmail.com>
2019-06-07 19:46:45 -0400

    nav changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -20,7 +20,7 @@ class Navigation extends React.Component {
               <i className="fa fa-align-right"></i>
             </Navbar.Toggle>
             <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
+              <Nav className="mr-auto" activeKey="1">
                 <Nav.Item>
                     <Link className="nav-link" activeClass="active"
                       to="Home"
