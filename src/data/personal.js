105352ad36475fe629f3900d9519ba1901cca282
Josh Sauder <joshsauder@gmail.com>
2019-06-10 21:20:12 -0400

    added content

--- src/data/personal.js
+++ src/data/personal.js
@@ -13,20 +13,20 @@ export const WhatIKnowList = [
 ]
 
 export const WhatImLearningList = [
-    "Currently - NodeJS, ReactJS, XCTest",
+    "Currently - NodeJS, ReactJS, XCTest, AWS Lambda",
     "Upcoming - MongoDB, Mocha, GraphQL, Gulp"
 ]
 
 export const PersonalList = [
     "I was born in Pettisville, OH (Toledo area), and graduated from Pettisville High School with a class of 50. Pretty small right!",
     "I then went to and graduated from The Ohio State University. As you can probably imagine, there was a bit of culture shock going from a small school to a school the size of OSU.",
-    "In my free-time you'll most likely find me, outside running. I'm sort of a self-proclaimed running fanatic. I've run 2 full marathons, 3 half marathons, and numerous 5K and 10K races. If you're wondering what I'm doing after work, there's a good chance I'm out running.",
+    "In my free-time you'll most likely find me outside running. I'm sort of a self-proclaimed running fanatic. I've run 2 full marathons, 3 half marathons, and numerous 5K and 10K races. If you're wondering what I'm doing after work, there's a good chance I'm out running.",
     "I also love traveling and exploring new places. It forces me to get out of my confort zone and exposes me to new cuisines, and experiences. It also allows me to venture around and see various places and landmarks.",
     "Finally, I'm a huge Ohio State Buckeye fan. I love watching OSU football and basketball whether I'm at the game, or watching it on TV."
 ]
 
 export const VolunteerWorkList = [
     "I've always been a firm believer that volunteer work is a vital aspect of a persons life. Not only for the personal benefits, but also because it benefits the communities involved.",
-    "I've always enjoyed helping those with special needs, and always loved participating in athletics. I also noticed that those with special needs werent't always given the opportunity to participate in athletics earlier.",
+    "I've always enjoyed helping those with special needs, and always loved participating in athletics. I also noticed that those with special needs werent't always given the opportunity to participate in athletics earlier in life.",
     "I recently started volunteering at Special Olympic events and found it to be an awesome experience watching these individuals do things that others told them they couldn't. The encouragement and support they are given by the fans and volunteers is a fun sight to see, and gives the athletes an environment were they can be proud of there achievements."
 ]
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 export const InTheClearList = [
-    "This app allows the user to determine what weather conditions they will be experiencing during their travels.",
+    "This app allows the user to determine what weather conditions they should expect to be experiencing during their travels.",
     "It includes a personalized route line that is colored based on the weather conditions, and a table that shows the user, city-by-city, what weather and temperatures they should expect.",
-    "I've always had a fascination with meterology. A common problem with living in the midwest is that when you travel, you can experience a variety of different weather conditions. This app solves this issue by telling the user what types of weather they will experience during their travels.",
+    "I've always had a fascination with meterology. A common problem with living in the midwest is that when you travel, you can experience a variety of different weather conditions, some of which are not safe to be driving in. This app solves this issue by telling the user what types of weather they will experience during their travels.",
     "Built using Swift",
     "Podfiles used include Google Maps, Google Places, AlamoFire, and SwiftlyJSON"
 ]
@@ -15,7 +15,7 @@ export const PersonalWebsiteList = [
 
 export const InTheClearWebList = [
     "A web app that will give the user all the functionality of the In The Clear iOS app.",
-    "It will be built using ReactJS, NodeJS, and most likely MongoDB.",
+    "It will be built using ReactJS, NodeJS, AWS Lambda, and most likely MongoDB.",
     "It will also use Google Maps and Google Places similar to the iOS App.",
     "Since this app isn't free, I will need to implement a user sign-in component (using MongoDB) on both the web app and iOS app."
 ]
