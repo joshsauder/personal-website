358b5e4cdcbe22eca1a664d58b30587a167cdfef
Josh Sauder <joshsauder@gmail.com>
2019-12-18 23:05:52 -0500

    icons now placed correctly

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,13 +12,26 @@ Populates each Featurette with paragraph items
 function PopulateList(props){
   return props.list.slice(0,4).map((item, index) =>
     <div className="lead col-md-3" key={index}>
-      <FontAwesomeIcon icon={faServer} />
+      <FontAwesomeIcon icon={determineIcon(item.company)} />
       <h4 className="text-center">{item.title}</h4> 
       <p>{item.company}</p>
       <p>{item.date}</p>
       <Button variant="outline-secondary" onClick={() => {this.setState({[item.modal]: true})}}>More Info</Button>
     </div>
   )
+}
+
+function determineIcon(job) {
+  switch(job){
+    case "Network Technologies International":
+      return faServer
+    case "OpenDemia":
+      return faGraduationCap
+    case "Chemical Abstract Services":
+      return faFlask
+    case "XPO Logistics":
+      return faShippingFast
+  }
 }
 
 
